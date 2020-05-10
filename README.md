11fd0ce942094f0e298588ce2482f6fd3bb6edfa
alesimattia <alesimattia@gmail.com>
2020-05-10 20:38:55 +0200

    Modifiche

--- README.md
+++ README.md
@@ -11,16 +11,21 @@ La struttura base è *laraProj5* quindi ci sono dei file che ancora non usiamo. 
 <br>
 
 A causa del framework, c'è una serie di linee guida da rispettare:
-- Il file base dove verrà iniettato tutto il contenuto è  ***layout.blade.php*** -> corrisponde all'index senza il contenuto centrale che ora si chiama ***home.blade.php*** <br>
-  Quindi la pagina iniziale è composta da *(layout.blade+home.blade)*
-- I file di layout che creiamo **NON DEVONO AVERE** i tag <code> html</code> e <code>body</code> ma direttamente "le div" con il contenuto.
-  
+- Il file base da cui creiamo tutte le viste è  ***layout_base*** -> corrisponde al vecchio index senza il contenuto centrale che ora è in ***home.blade.php***
+  - Quindi la pagina iniziale si compone (creando una nuova vista) prendendo gli elementi da *layout_base.html* e aggiungendo *home.blade.php*
+  - Si può fare copia-incolla dal layout_base per creare una nuova vista (come ad esempio *public.blade.php*)
+  <br>
+- I file che creiamo, se hanno l'obiettivo di essere iniettati (in una vista in /layouts)  **NON DEVONO AVERE** i tag <code>html</code> e <code>body</code> ma direttamente "le div" con il contenuto --> sarebbero quelli fuori da */views/layouts*
+<br>
 - Tutti i file delle viste devono avere formato ***.blade.php*** (anche contengono principalmente codice html)
   - In */resources/views/layouts*  &nbsp; inseriamo tutte le viste
   - In */resources/views* salviamo le viste "generiche" come:&nbsp; *registrati*, *login*, *chiSiamo* e *doveSiamo*
-
+<br>
 - I fogli di stile, le immagini e comunque altre risorse (js) che le pagine usano devono stare in  */public/*
   
-Non date nomi strani ai file (tipo &nbsp;catalogoMattia.blade) perchè non è necessario distinguere i file: le modifiche e chi le ha fatte le evidenzia Github --> Date un nome usabile all'interno dell'app.
-<br><br>
+Non date nomi strani ai file (tipo &nbsp;*catalogoMattia.blade.php*) perchè <u>non è necessario distinguere i file</u>:<br> le modifiche e chi le ha fatte le evidenzia Github --> Date un nome usabile all'interno dell'app.
+
 Nella cartella ALTRO ho spostao i file utili a noi ma non all'app.
+
+**Per adesso si genera un errore perchè *laraProj5* richiedeva un database. 
+Devo trovare il modo di disattivarlo temporaneamente.** 
--- resources/views/layouts/nav_amm.blade.php
+++ resources/views/layouts/nav_amm.blade.php
@@ -1,67 +1,48 @@
-<!DOCTYPE html>
-<html lang="it">
+<header class="header_area">
+    <div class="main_menu">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light fixed">   <!-- eventualemte non trasparente style="background-color: #fff;-->
+            <div class="container">
+                <a class="navbar-brand logo_h" href="index.html"><img src="img/home/main_logo.png"></a>  <!-- LOGO OTTIMO, NON CAMBIARE-->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Electro - Shop</title>
+                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
+                        <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="catalogo.html">Catalogo</a></li>                           
+                        <li class="nav-item submenu dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                                aria-expanded="false">Gestisci prodotti</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="#">Inserisci prodotti</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Modifica prodotti</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Cancella prodotti</a></li>                                  
+                            </ul>
+                        </li>
+                        <li class="nav-item submenu dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                                aria-expanded="false">Gestisci Staff</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="#">Inserisci utente staff</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Modifica staff</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Cancella utente staff</a></li>                                  
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="#">Elimina profilo Cliente</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Modifica profilo</a></li>
+                    </ul>
+    
+                    <ul class="nav-shop">
+                        <li class="nav-item"><a class="button button-header button-navbar" href="#">Logout</a></li>
+                    </ul>
 
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-
-
-    <header class="header_area">
-        <div class="main_menu">
-            <!-- Navbar -->
-            <nav class="navbar navbar-expand-lg navbar-light fixed">   <!-- eventualemte non trasparente style="background-color: #fff;-->
-                <div class="container">
-                    <a class="navbar-brand logo_h" href="index.html"><img src="img/home/main_logo.png"></a>  <!-- LOGO OTTIMO, NON CAMBIARE-->
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
-                            <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
-                            <li class="nav-item"><a class="nav-link" href="catalogo.html">Catalogo</a></li>                           
-                            <li class="nav-item submenu dropdown">
-                               <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                                  aria-expanded="false">Gestisci prodotti</a>
-                               <ul class="dropdown-menu">
-                                   <li class="nav-item"><a class="nav-link" href="#">Inserisci prodotti</a></li>
-                                   <li class="nav-item"><a class="nav-link" href="#">Modifica prodotti</a></li>
-                                   <li class="nav-item"><a class="nav-link" href="#">Cancella prodotti</a></li>                                  
-                               </ul>
-                            </li>
-                            <li class="nav-item submenu dropdown">
-                               <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                                  aria-expanded="false">Gestisci Staff</a>
-                               <ul class="dropdown-menu">
-                                   <li class="nav-item"><a class="nav-link" href="#">Inserisci utente staff</a></li>
-                                   <li class="nav-item"><a class="nav-link" href="#">Modifica staff</a></li>
-                                   <li class="nav-item"><a class="nav-link" href="#">Cancella utente staff</a></li>                                  
-                               </ul>
-                            </li>
-                            <li class="nav-item"><a class="nav-link" href="#">Elimina profilo Cliente</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">Modifica profilo</a></li>
-                        </ul>
-        
-                        <ul class="nav-shop">
-                            <li class="nav-item"><a class="button button-header button-navbar" href="#">Logout</a></li>
-                        </ul>
-
-                    </div>
                 </div>
-            </nav>
-            <!--Fine elementi Navbar-->
-        </div>
-    </header>
-    <!--================ End Header Menu Area =================-->
-</body>
-
-</html>
+            </div>
+        </nav>
+        <!--Fine elementi Navbar-->
+    </div>
+</header>
